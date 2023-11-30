a3a4b0433c1e8b35a0f27be1814ab5c91e35f39f
Tarcizo Neto <tarcizon12@gmail.com>
2023-11-30 07:57:27 -0300

    Iniciando hillClimbing

---
+++ Primeira Atividade/Algoritmos.py
@@ -0,0 +1,10 @@
+def hillClimbing(x1,x2,funcao):
+    xBest = 1
+    candidado = xBest
+
+    debugarPrint('Melhor X', xBest)
+    
+
+
+def debugarPrint(flag, variavel):
+    print('Dubug-> ', flag, variavel)
---
+++ Primeira Atividade/main.py
@@ -0,0 +1,11 @@
+import Algoritmos
+
+def Main():
+  print('Trabalho de AV3')
+  
+  Algoritmos.hillClimbing(1,2,3)
+
+
+
+
+Main()
